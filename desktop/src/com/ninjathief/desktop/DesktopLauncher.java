c8d6b7c1062cb16f23cb4aa75c2dfc2bc8dd3afb
danin0s <daninos04@gmail.com>
2015-07-04 23:28:11 +0300

    Initial commit

---
+++ desktop/src/com/ninjathief/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ninjathief.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ninjathief.NinjaThief;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NinjaThief(), config);
+	}
+}
