cf76991e3b47b0b2b56a8933c23dd759696384f1
yaronmiron <yaron795@walla.com>
2015-07-05 00:33:49 +0300

    sdasdasasd

--- core/src/com/ninjathief/NinjaThief.java
+++ core/src/com/ninjathief/NinjaThief.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+//dfsdfsdfs
 public class NinjaThief extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
